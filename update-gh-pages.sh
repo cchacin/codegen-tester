0addff05843a52f8acd81a26dab4f3258d02c015
Carlos Chacin <cchacin@gmail.com>
2017-10-21 10:57:24 -0700

    Adding deps

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cchacin/cucumber-common-steps.git gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/cchacin/codegen-tester.git gh-pages > /dev/null
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
